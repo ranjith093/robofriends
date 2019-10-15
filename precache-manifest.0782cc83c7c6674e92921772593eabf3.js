086ad322bdedc90a29194c850c17e2f0a64f9a45
ranjith093 <ranjithnair093@gmail.com>
2019-10-15 18:38:15 +0530

    Updates

---
+++ precache-manifest.0782cc83c7c6674e92921772593eabf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb33165ddcc6be2104bc4aac0f023d15",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1692e2135af89f3e98f9",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a74591136a03c6bdc785",
+    "url": "/robofriends/static/css/main.658c6bd6.chunk.css"
+  },
+  {
+    "revision": "1692e2135af89f3e98f9",
+    "url": "/robofriends/static/js/2.96da6c05.chunk.js"
+  },
+  {
+    "revision": "a74591136a03c6bdc785",
+    "url": "/robofriends/static/js/main.33e0bbe2.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
